7a755751ddbab269c2bab0a07a0ad55893328630
keithfilegar <keith.filegar@gmail.com>
2021-02-23 15:13:31 -0800

    wire up delete button on list items

--- src/App.js
+++ src/App.js
@@ -8,46 +8,17 @@ import config from './config';
 import './App.css';
 import BookmarksContext from './BookmarksContext';
 
-const bookmarks = [
-  // {
-  //   id: 0,
-  //   title: 'Google',
-  //   url: 'http://www.google.com',
-  //   rating: '3',
-  //   desc: 'Internet-related services and products.'
-  // },
-  // {
-  //   id: 1,
-  //   title: 'Thinkful',
-  //   url: 'http://www.thinkful.com',
-  //   rating: '5',
-  //   desc: '1-on-1 learning to accelerate your way to a new high-growth tech career!'
-  // },
-  // {
-  //   id: 2,
-  //   title: 'Github',
-  //   url: 'http://www.github.com',
-  //   rating: '4',
-  //   desc: 'brings together the world\'s largest community of developers.'
-  // }
-];
 
 class App extends Component {
   state = {
-    page: 'list',
-    bookmarks,
+    bookmarks: [],
     error: null,
   };
-
-  changePage = (page) => {
-    this.setState({ page })
-  }
 
   setBookmarks = bookmarks => {
     this.setState({
       bookmarks,
       error: null,
-      page: 'list',
     })
   }
 
@@ -63,6 +34,13 @@ class App extends Component {
     )
     this.setState({
       bookmarks: newBookmarks
+    })
+  }
+
+  updateBookmark = updatedBookmark => {
+    this.setState({
+      bookmarks: this.state.bookmarks.map(bm =>
+        (bm.id !== updatedBookmark.id) ? bm : updatedBookmark)
     })
   }
 
